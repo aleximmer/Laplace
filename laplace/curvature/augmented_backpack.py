bdf64f3d7c4b0967ea902270cee6af3c94f65921
Alex Immer <AlexImmer@users.noreply.github.com>
2021-08-24 12:01:48 +0200

    Support augmentation: average functions and compute their Jacobians

--- laplace/curvature/augmented_backpack.py
+++ laplace/curvature/augmented_backpack.py
@@ -1,10 +1,10 @@
 import torch
 
-from backpack import backpack, extend, memory_cleanup
+from backpack import backpack, extend
 from backpack.extensions import BatchGrad
 from backpack.context import CTX
 
-from laplace.curvature import CurvatureInterface, GGNInterface, EFInterface
+from laplace.curvature import CurvatureInterface, GGNInterface
 from laplace.curvature.backpack import _cleanup
 
 
@@ -20,7 +20,6 @@ class AugBackPackInterface(CurvatureInterface):
 
     @staticmethod
     def jacobians(model, x):
-        # TODO: what should this return for x being `(batch, S, input_shape)`?
         """Compute Jacobians \\(\\nabla_{\\theta} f(x;\\theta)\\) at current parameter \\(\\theta\\)
         using backpack's BatchGrad per output dimension.
 
@@ -28,15 +27,18 @@ class AugBackPackInterface(CurvatureInterface):
         ----------
         model : torch.nn.Module
         x : torch.Tensor
-            input data `(batch, input_shape)` on compatible device with model.
+            input data `(batch, n_augs, input_shape)` on compatible device with model.
 
         Returns
         -------
         Js : torch.Tensor
-            Jacobians `(batch, parameters, outputs)`
+            averaged Jacobians over `n_augs` of shape `(batch, parameters, outputs)`
         f : torch.Tensor
-            output function `(batch, outputs)`
+            averaged output function over `n_augs` of shape `(batch, outputs)`
         """
+        batch_size, n_augs = x.shape[:2]
+        x_aug = x
+        x = x.flatten(start_dim=0, end_dim=1)
         model = extend(model)
         to_stack = []
         for i in range(model.output_size):
@@ -51,12 +53,16 @@ class AugBackPackInterface(CurvatureInterface):
                 for param in model.parameters():
                     to_cat.append(param.grad_batch.reshape(x.shape[0], -1))
                     delattr(param, 'grad_batch')
-                Jk = torch.cat(to_cat, dim=1)
+                Jk = torch.cat(to_cat, dim=1).reshape(batch_size, n_augs, -1).mean(dim=1)
             to_stack.append(Jk)
             if i == 0:
-                f = out
+                f = out.reshape(batch_size, n_augs, -1).mean(dim=1)
 
+        # set gradients to zero, differentiation here only serves Jacobian computation
         model.zero_grad()
+        if x_aug.grad is not None:
+            x_aug.grad.zero_()
+
         CTX.remove_hooks()
         _cleanup(model)
         if model.output_size > 1:
@@ -65,29 +71,8 @@ class AugBackPackInterface(CurvatureInterface):
             return Jk.unsqueeze(-1).transpose(1, 2), f
 
     def gradients(self, x, y):
-        # TODO: what should this return for x being `(batch, S, input_shape)`?
-        """Compute gradients \\(\\nabla_\\theta \\ell(f(x;\\theta, y)\\) at current parameter
-        \\(\\theta\\) using Backpack's BatchGrad.
-
-        Parameters
-        ----------
-        x : torch.Tensor
-            input data `(batch, input_shape)` on compatible device with model.
-        y : torch.Tensor
-
-        Returns
-        -------
-        loss : torch.Tensor
-        Gs : torch.Tensor
-            gradients `(batch, parameters)`
-        """
-        f = self.model(x)
-        loss = self.lossfunc(f, y)
-        with backpack(BatchGrad()):
-            loss.backward(retain_graph=True, create_graph=True)
-        Gs = torch.cat([p.grad_batch.data.flatten(start_dim=1)
-                        for p in self._model.parameters()], dim=1)
-        return Gs, loss
+        # Problem: averaging leads to shape issues with backpack here.
+        raise NotImplementedError
 
 
 class AugBackPackGGN(AugBackPackInterface, GGNInterface):
@@ -98,7 +83,6 @@ class AugBackPackGGN(AugBackPackInterface, GGNInterface):
         self.stochastic = stochastic
 
     def full(self, x, y, **kwargs):
-        # TODO: what should this return for x being `(batch, S, input_shape)`?
         """Compute the full GGN \\(P \\times P\\) matrix as Hessian approximation
         \\(H_{ggn}\\) with respect to parameters \\(\\theta \\in \\mathbb{R}^P\\).
         For last-layer, reduced to \\(\\theta_{last}\\)
@@ -106,7 +90,7 @@ class AugBackPackGGN(AugBackPackInterface, GGNInterface):
         Parameters
         ----------
         x : torch.Tensor
-            input data `(batch, input_shape)`
+            input data `(batch, n_augs, input_shape)`
         y : torch.Tensor
             labels `(batch, label_shape)`
 
@@ -125,51 +109,9 @@ class AugBackPackGGN(AugBackPackInterface, GGNInterface):
         loss, H_ggn = self._get_full_ggn(Js, f, y)
 
         return loss, H_ggn
-    
+
     def diag(self, X, y, **kwargs):
         raise NotImplementedError('Unavailable for DA.')
 
     def kron(self, X, y, N, **kwargs):
         raise NotImplementedError('Unavailable for DA.')
-
-
-class AugBackPackEF(AugBackPackInterface, EFInterface):
-    """Implementation of `EFInterface` using Backpack.
-    """
-
-    def full(self, x, y, **kwargs):
-        # TODO: what should this return for x being `(batch, S, input_shape)`?
-        """Compute the full EF \\(P \\times P\\) matrix as Hessian approximation
-        \\(H_{ef}\\) with respect to parameters \\(\\theta \\in \\mathbb{R}^P\\).
-        For last-layer, reduced to \\(\\theta_{last}\\)
-
-        Parameters
-        ----------
-        x : torch.Tensor
-            input data `(batch, input_shape)`
-        y : torch.Tensor
-            labels `(batch, label_shape)`
-
-        Returns
-        -------
-        loss : torch.Tensor
-        H_ef : torch.Tensor
-            EF `(parameters, parameters)`
-        """
-        Gs, loss = self.gradients(x, y)
-        H_ef = Gs.T @ Gs
-        return self.factor * loss.detach(), self.factor * H_ef
-
-    def kron(self, X, y, **kwargs):
-        raise NotImplementedError('Unavailable through Backpack.')
-
-    def diag(self, X, y, **kwargs):
-        raise NotImplementedError('Unavailable for DA.')
-
-
-def _cleanup(module):
-    for child in module.children():
-        _cleanup(child)
-
-    setattr(module, "_backpack_extend", False)
-    memory_cleanup(module)
