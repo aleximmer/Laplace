a793a0457e3e8c6a1856e7f22c3995303a2bcb97
metodmove <metod.jazbec@movedigital.ch>
2021-09-25 08:12:25 +0200

    test small refactor

--- tests/test_functional_laplace.py
+++ tests/test_functional_laplace.py
@@ -1,45 +1,19 @@
 import numpy as np
-import pytest
 import torch
-from torch import nn
-from torch.utils.data import DataLoader, TensorDataset
 from laplace.baselaplace import FullLaplace, FunctionalLaplace
+from tests.utils import toy_model, toy_regression_dataset
 
-
-n_epochs = 100
-batch_size = 150  # full batch
 true_sigma_noise = 0.3
 torch.manual_seed(711)
 
-# create simple sinusoid data set
-X_train = (torch.rand(150) * 8).unsqueeze(-1)
-y_train = torch.sin(X_train) + torch.randn_like(X_train) * true_sigma_noise
-train_loader = DataLoader(TensorDataset(X_train, y_train), batch_size=batch_size)
-X_test = torch.linspace(-5, 13, 500).unsqueeze(-1)  # +-5 on top of the training X-range
 
+def test_gp_equivalence():
+    X_train, y_train, train_loader, X_test = toy_regression_dataset(sigma=true_sigma_noise)
+    model = toy_model(train_loader)
 
-@pytest.fixture
-def fitted_model():
-    model = torch.nn.Sequential(torch.nn.Linear(1, 50),
-                                torch.nn.Tanh(),
-                                torch.nn.Linear(50, 1))
-
-    criterion = torch.nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), weight_decay=5e-4, lr=1e-2)
-    for i in range(n_epochs):
-        for X, y in train_loader:
-            optimizer.zero_grad()
-            loss = criterion(model(X), y)
-            loss.backward()
-            optimizer.step()
-
-    return model
-
-
-def test_gp_equivalence(fitted_model):
-    full_la = FullLaplace(fitted_model, 'regression', sigma_noise=true_sigma_noise)
+    full_la = FullLaplace(model, 'regression', sigma_noise=true_sigma_noise)
     full_la.fit(train_loader)
-    functional_gp_la = FunctionalLaplace(fitted_model, 'regression', M=len(X_train), sigma_noise=true_sigma_noise)
+    functional_gp_la = FunctionalLaplace(model, 'regression', M=len(X_train), sigma_noise=true_sigma_noise)
     functional_gp_la.fit(train_loader)
 
     f_mu_full, f_var_full = full_la(X_test)
--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,31 @@
 import torch
 import numpy as np
+from torch.utils.data import DataLoader, TensorDataset
+
+
+def toy_regression_dataset(sigma, n_train=150, n_test=500, batch_size=150):
+    # create simple sinusoid data set
+    X_train = (torch.rand(n_train) * 8).unsqueeze(-1)
+    y_train = torch.sin(X_train) + torch.randn_like(X_train) * sigma
+    train_loader = DataLoader(TensorDataset(X_train, y_train), batch_size=batch_size)
+    X_test = torch.linspace(-5, 13, n_test).unsqueeze(-1)  # +-5 on top of the training X-range
+    return X_train, y_train, train_loader, X_test
+
+
+def toy_model(train_loader: DataLoader, n_epochs=100, fit=True):
+    model = torch.nn.Sequential(torch.nn.Linear(1, 50),
+                                torch.nn.Tanh(),
+                                torch.nn.Linear(50, 1))
+    if fit:
+        criterion = torch.nn.MSELoss()
+        optimizer = torch.optim.Adam(model.parameters(), weight_decay=5e-4, lr=1e-2)
+        for i in range(n_epochs):
+            for X, y in train_loader:
+                optimizer.zero_grad()
+                loss = criterion(model(X), y)
+                loss.backward()
+                optimizer.step()
+    return model
 
 
 def get_psd_matrix(dim):
