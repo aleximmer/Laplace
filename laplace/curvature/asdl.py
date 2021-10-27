c604e750f99956b4e903fbd81731fcdfdf6e7209
Alex Immer <AlexImmer@users.noreply.github.com>
2021-10-27 23:03:14 +0200

    Enable regression with new ASDL version; update ASDL interface for new version

--- laplace/curvature/asdl.py
+++ laplace/curvature/asdl.py
@@ -2,9 +2,11 @@ import warnings
 import numpy as np
 import torch
 
-from asdfghjkl import FISHER_EXACT, FISHER_MC, COV
-from asdfghjkl import SHAPE_KRON, SHAPE_DIAG, SHAPE_FULL
-from asdfghjkl import fisher_for_cross_entropy
+from asdfghjkl import (
+    FISHER_EXACT, FISHER_MC, FISHER_EMP, SHAPE_KRON, SHAPE_DIAG, 
+    SHAPE_FULL, LOSS_MSE, LOSS_CROSS_ENTROPY
+)
+from asdfghjkl.fisher import calculate_fisher
 from asdfghjkl.hessian import hessian_eigenvalues, hessian_for_loss
 from asdfghjkl.gradient import batch_gradient
 
@@ -17,6 +19,10 @@ EPS = 1e-6
 class AsdlInterface(CurvatureInterface):
     """Interface for asdfghjkl backend.
     """
+
+    @property
+    def loss_type(self):
+        return LOSS_MSE if self.likelihood == 'regression' else LOSS_CROSS_ENTROPY
 
     def jacobians(self, x):
         """Compute Jacobians \\(\\nabla_\\theta f(x;\\theta)\\) at current parameter \\(\\theta\\)
@@ -39,11 +45,8 @@ class AsdlInterface(CurvatureInterface):
             def loss_fn(outputs, targets):
                 return outputs[:, i].sum()
 
-            f = batch_gradient(self.model, loss_fn, x, None).detach()
-            Jk = _get_batch_grad(self.model)
-            if self.subnetwork_indices is not None:
-                Jk = Jk[:, self.subnetwork_indices]
-            Js.append(Jk)
+            Jsi, f = batch_gradient(self.model, loss_fn, x, None)
+            Js.append(Jsi)
         Js = torch.stack(Js, dim=1)
         return Js, f
 
@@ -63,10 +66,7 @@ class AsdlInterface(CurvatureInterface):
         Gs : torch.Tensor
             gradients `(batch, parameters)`
         """
-        f = batch_gradient(self.model, self.lossfunc, x, y).detach()
-        Gs = _get_batch_grad(self._model)
-        if self.subnetwork_indices is not None:
-            Gs = Gs[:, self.subnetwork_indices]
+        Gs, f = batch_gradient(self.model, self.lossfunc, x, y)
         loss = self.lossfunc(f, y)
         return Gs, loss
 
@@ -112,12 +112,11 @@ class AsdlInterface(CurvatureInterface):
             else:
                 f = self.model(X)
             loss = self.lossfunc(f, y)
-        curv = fisher_for_cross_entropy(self._model, self._ggn_type, SHAPE_DIAG,
-                                        inputs=X, targets=y)
+        curv = calculate_fisher(self._model, self.loss_type, self._ggn_type, SHAPE_DIAG, 
+                                data_average=False, inputs=X, targets=y)
         diag_ggn = curv.matrices_to_vector(None)
-        if self.subnetwork_indices is not None:
-            diag_ggn = diag_ggn[self.subnetwork_indices]
-        return self.factor * loss, self.factor * diag_ggn
+        curv_factor = 1.0  # ASDL uses proper 1/2 * MSELoss
+        return self.factor * loss, curv_factor * diag_ggn
 
     def kron(self, X, y, N, **wkwargs):
         with torch.no_grad():
@@ -126,12 +125,13 @@ class AsdlInterface(CurvatureInterface):
             else:
                 f = self.model(X)
             loss = self.lossfunc(f, y)
-        curv = fisher_for_cross_entropy(self._model, self._ggn_type, SHAPE_KRON,
-                                        inputs=X, targets=y)
+        curv = calculate_fisher(self._model, self.loss_type, self._ggn_type, SHAPE_KRON, 
+                                data_average=False, inputs=X, targets=y)
         M = len(y)
         kron = self._get_kron_factors(curv, M)
         kron = self._rescale_kron_factors(kron, N)
-        return self.factor * loss, self.factor * kron
+        curv_factor = 1.0  # ASDL uses proper 1/2 * MSELoss
+        return self.factor * loss, curv_factor * kron
 
 
 class AsdlHessian(AsdlInterface):
@@ -188,25 +188,4 @@ class AsdlEF(AsdlInterface, EFInterface):
 
     @property
     def _ggn_type(self):
-        return COV
-
-
-def _flatten_after_batch(tensor: torch.Tensor):
-    if tensor.ndim == 1:
-        return tensor.unsqueeze(-1)
-    else:
-        return tensor.flatten(start_dim=1)
-
-
-def _get_batch_grad(model):
-    batch_grads = list()
-    for module in model.modules():
-        if hasattr(module, 'op_results'):
-            res = module.op_results['batch_grads']
-            if 'weight' in res:
-                batch_grads.append(_flatten_after_batch(res['weight']))
-            if 'bias' in res:
-                batch_grads.append(_flatten_after_batch(res['bias']))
-            if len(set(res.keys()) - {'weight', 'bias'}) > 0:
-                raise ValueError(f'Invalid parameter keys {res.keys()}')
-    return torch.cat(batch_grads, dim=1)
+        return FISHER_EMP
