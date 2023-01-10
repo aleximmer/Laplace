39000ae7f79fad11b735e50cd6235b65183bd6c8
Metod Jazbec <metodjazbec@uvavpn-byodm-145-18-167-80.vpn.uva.nl>
2023-01-10 20:32:18 +0100

    minor2

--- examples/calibration_gp_example.py
+++ examples/calibration_gp_example.py
@@ -25,11 +25,11 @@ DATASET = 'FMNIST'
 MODEL_NAME = 'FMNIST_CNN_10_2.2e+02.pt'
 
 ds_train, ds_test = get_dataset(DATASET, False, 'cuda')
-train_loader = DataLoader(ds_train, batch_size=128, shuffle=True)
-test_loader = DataLoader(ds_test, batch_size=100, shuffle=False)
+train_loader = DataLoader(ds_train, batch_size=48, shuffle=True)
+test_loader = DataLoader(ds_test, batch_size=24, shuffle=False)
 targets = torch.cat([y for x, y in test_loader], dim=0).cpu()
 
-model = CIFAR10Net(ds_train.channels, ds_train.K, use_tanh=True)
+model = CIFAR10Net(ds_train.channels, ds_train.K, use_tanh=True).to('cuda')
 state = torch.load(f'helper/models/{MODEL_NAME}')
 model.load_state_dict(state['model'])
 model = model.cuda()
@@ -45,8 +45,8 @@ for m in [50, 200, 800, 1600]:
         la.fit(train_loader)
 
         probs_laplace = predict(test_loader, la, laplace=True, la_type='gp')
-        acc_laplace = (probs_laplace.argmax(-1) == targets).float().mean()
-        ece_laplace = ECE(bins=15).measure(probs_laplace.numpy(), targets.numpy()).cpu().item()
+        acc_laplace = (probs_laplace.argmax(-1) == targets).float().mean().cpu().item()
+        ece_laplace = ECE(bins=15).measure(probs_laplace.numpy(), targets.numpy())
         nll_laplace = -dists.Categorical(probs_laplace).log_prob(targets).mean().cpu().item()
 
         print(f'[Laplace-GP, m={m}] Acc.: {acc_laplace:.1%}; ECE: {ece_laplace:.1%}; NLL: {nll_laplace:.3}')
