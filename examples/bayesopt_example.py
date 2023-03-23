Fix backprop through Jacobians; Make detach optional; Add tests

examples/bayesopt_example.py
@@ -88,7 +88,7 @@ class LaplaceBNN(Model):
 
         # Posterior predictive distribution
         # mean_y is (batch_shape*q, k); cov_y is (batch_shape*q*k, batch_shape*q*k)
-        mean_y, cov_y = self._get_prediction(X, joint=True)
+        mean_y, cov_y = self._get_prediction(X, use_test_loader=False)
 
         # Mean in `(batch_shape, q*k)`
         K = self.num_outputs
@@ -152,7 +152,7 @@ class LaplaceBNN(Model):
             subset_of_weights='all', hessian_structure='kron',
         )
         self.bnn.fit(train_loader)
-        self.bnn.optimize_prior_precision(n_steps=30)
+        self.bnn.optimize_prior_precision(n_steps=100)
 
 
     def _get_prediction(self, test_x: torch.Tensor, joint=True, use_test_loader=False):
@@ -167,9 +167,10 @@ class LaplaceBNN(Model):
         """
         if self.bnn is None:
             print('Train your model first before making prediction!')
+            sys.exit(1)
 
         if not use_test_loader:
-            mean_y, cov_y = self.bnn(test_x, joint=joint)
+            mean_y, cov_y = self.bnn(test_x, joint=True, detach=False)
         else:
             test_loader = data_utils.DataLoader(
                 data_utils.TensorDataset(test_x, torch.zeros_like(test_x)),
@@ -179,7 +180,7 @@ class LaplaceBNN(Model):
             mean_y, cov_y = [], []
 
             for x_batch, _ in test_loader:
-                _mean_y, _cov_y = self.bnn(x_batch, joint=joint)
+                _mean_y, _cov_y = self.bnn(x_batch, joint=joint, detach=False)
                 mean_y.append(_mean_y)
                 cov_y.append(_cov_y)
 

laplace/baselaplace.py
@@ -497,7 +497,7 @@ class ParametricLaplace(BaseLaplace):
         return self.log_likelihood - 0.5 * (self.log_det_ratio + self.scatter)
 
     def __call__(self, x, pred_type='glm', joint=False, link_approx='probit', 
-                 n_samples=100, diagonal_output=False, generator=None):
+                 n_samples=100, diagonal_output=False, generator=None, detach=True):
         """Compute the posterior predictive on input data `x`.
 
         Parameters
@@ -519,6 +519,7 @@ class ParametricLaplace(BaseLaplace):
             `pred_type='glm'`. If set to `True`, the predictive distribution
             has the same form as GP posterior, i.e. N([f(x1), ...,f(xm)], Cov[f(x1), ..., f(xm)]).
             If `False`, then only outputs the marginal predictive distribution. 
+            Only available for regression and GLM predictive.
 
         n_samples : int
             number of samples for `link_approx='mc'`.
@@ -529,6 +530,9 @@ class ParametricLaplace(BaseLaplace):
 
         generator : torch.Generator, optional
             random number generator to control the samples (if sampling used)
+
+        detach : bool, default = True
+            Detach the resulting prediction. Set to True to enable backprop.
 
         Returns
         -------
@@ -554,7 +558,9 @@ class ParametricLaplace(BaseLaplace):
                 raise ValueError('Invalid random generator (check type and device).')
 
         if pred_type == 'glm':
-            f_mu, f_var = self._glm_predictive_distribution(x, joint=joint)
+            f_mu, f_var = self._glm_predictive_distribution(
+                x, joint=joint and self.likelihood == 'regression', detach=detach
+            )
             # regression
             if self.likelihood == 'regression':
                 return f_mu, f_var
@@ -584,7 +590,7 @@ class ParametricLaplace(BaseLaplace):
                 alpha = (1 - 2/K + f_mu.exp() / K**2 * sum_exp) / f_var_diag
                 return torch.nan_to_num(alpha / alpha.sum(dim=1).unsqueeze(-1), nan=1.0)
         else:
-            samples = self._nn_predictive_samples(x, n_samples)
+            samples = self._nn_predictive_samples(x, n_samples, detach=detach)
             if self.likelihood == 'regression':
                 return samples.mean(dim=0), samples.var(dim=0)
             return samples.mean(dim=0)
@@ -636,8 +642,8 @@ class ParametricLaplace(BaseLaplace):
             return self._nn_predictive_samples(x, n_samples)
 
     @torch.enable_grad()
-    def _glm_predictive_distribution(self, X, joint=False):
-        Js, f_mu = self.backend.jacobians(X)
+    def _glm_predictive_distribution(self, X, joint=False, detach=True):
+        Js, f_mu = self.backend.jacobians(X, enable_backprop=not detach)
 
         if joint:
             f_mu = f_mu.flatten()  # (batch*out)
@@ -645,13 +651,14 @@ class ParametricLaplace(BaseLaplace):
         else:
             f_var = self.functional_variance(Js)
 
-        return f_mu, f_var
+        return (f_mu.detach(), f_var.detach()) if detach else (f_mu, f_var)
 
-    def _nn_predictive_samples(self, X, n_samples=100):
+    def _nn_predictive_samples(self, X, n_samples=100, detach=True):
         fs = list()
         for sample in self.sample(n_samples):
             vector_to_parameters(sample, self.model.parameters())
-            fs.append(self.model(X.to(self._device)).detach())
+            f = self.model(X.to(self._device))
+            fs.append(f.detach() if detach else f)
         vector_to_parameters(self.mean, self.model.parameters())
         fs = torch.stack(fs)
         if self.likelihood == 'classification':

laplace/curvature/backpack.py
@@ -16,7 +16,7 @@ class BackPackInterface(CurvatureInterface):
         extend(self._model)
         extend(self.lossfunc)
 
-    def jacobians(self, x):
+    def jacobians(self, x, enable_backprop=False):
         """Compute Jacobians \\(\\nabla_{\\theta} f(x;\\theta)\\) at current parameter \\(\\theta\\)
         using backpack's BatchGrad per output dimension.
 
@@ -24,6 +24,9 @@ class BackPackInterface(CurvatureInterface):
         ----------
         x : torch.Tensor
             input data `(batch, input_shape)` on compatible device with model.
+
+        enable_backprop : bool, default = False
+            whether to enable backprop through the Js and f w.r.t. x
 
         Returns
         -------
@@ -39,12 +42,18 @@ class BackPackInterface(CurvatureInterface):
             out = model(x)
             with backpack(BatchGrad()):
                 if model.output_size > 1:
-                    out[:, i].sum().backward()
+                    out[:, i].sum().backward(
+                        create_graph=enable_backprop, 
+                        retain_graph=enable_backprop
+                    )
                 else:
-                    out.sum().backward(retain_graph=True)
+                    out.sum().backward(
+                        create_graph=enable_backprop, 
+                        retain_graph=enable_backprop
+                    )
                 to_cat = []
                 for param in model.parameters():
-                    to_cat.append(param.grad_batch.detach().reshape(x.shape[0], -1))
+                    to_cat.append(param.grad_batch.reshape(x.shape[0], -1))
                     delattr(param, 'grad_batch')
                 Jk = torch.cat(to_cat, dim=1)
                 if self.subnetwork_indices is not None:

tests/test_baselaplace.py
@@ -1,4 +1,4 @@
-from math import sqrt
+from math import sqrt, prod
 import pytest
 from itertools import product
 import numpy as np
@@ -355,16 +355,28 @@ def test_regression_predictive(laplace, model, reg_loader):
         lap(X, pred_type='linear')
 
     # GLM predictive, functional variance tested already above.
-    f_mu, f_var = lap(X, pred_type='glm')
-    assert torch.allclose(f_mu, f)
-    assert f_var.shape == torch.Size([f_mu.shape[0], f_mu.shape[1], f_mu.shape[1]])
-    assert len(f_mu) == len(X)
+    f_mu_glm, f_var_glm = lap(X, pred_type='glm')
+    assert torch.allclose(f_mu_glm, f)
+    assert f_var_glm.shape == torch.Size([f_mu_glm.shape[0], f_mu_glm.shape[1], f_mu_glm.shape[1]])
+    assert len(f_mu_glm) == len(X)
 
     # NN predictive (only diagonal variance estimation)
-    f_mu, f_var = lap(X, pred_type='nn', link_approx='mc')
-    assert f_mu.shape == f_var.shape
-    assert f_var.shape == torch.Size([f_mu.shape[0], f_mu.shape[1]])
-    assert len(f_mu) == len(X)
+    f_mu_nn, f_var_nn = lap(X, pred_type='nn', link_approx='mc')
+    assert f_mu_nn.shape == f_var_nn.shape
+    assert f_var_nn.shape == torch.Size([f_mu_nn.shape[0], f_mu_nn.shape[1]])
+    assert len(f_mu_nn) == len(X)
+
+    # Test joint prediction 
+    f_mu_joint, f_cov_joint = lap(X, pred_type='glm', joint=True)
+    assert len(f_mu_joint.shape) == 1
+    assert f_mu_joint.shape[0] == prod(f_mu_glm.shape)
+    assert len(f_cov_joint.shape) == 2
+    assert f_cov_joint.shape == (f_mu_joint.shape[0], f_mu_joint.shape[0])
+
+    # The "diagonal" of the joint cov should equal the non-joint var
+    b, k = y.shape
+    f_var_joint = torch.einsum('bkbl->bkl', f_cov_joint.reshape(b, k, b, k))
+    assert torch.allclose(f_var_joint, f_var_glm)
 
 
 @pytest.mark.parametrize('laplace', flavors)