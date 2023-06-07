239bea6b8595ca1caa26a51f56d18de0cc078eba
Agustinus Kristiadi <agustinus@kristia.de>
2023-06-07 11:05:36 -0400

    Revert back KronLaplace initialization

--- laplace/baselaplace.py
+++ laplace/baselaplace.py
@@ -818,11 +818,11 @@ class KronLaplace(ParametricLaplace):
 
     def __init__(self, model, likelihood, sigma_noise=1., prior_precision=1.,
                  prior_mean=0., temperature=1., backend=None, damping=False,
-                 backend_kwargs=None):
+                 **backend_kwargs):
         self.damping = damping
         self.H_facs = None
         super().__init__(model, likelihood, sigma_noise, prior_precision,
-                         prior_mean, temperature, backend, backend_kwargs=backend_kwargs)
+                         prior_mean, temperature, backend, **backend_kwargs)
 
     def _init_H(self):
         self.H = Kron.init_from_model(self.params, self._device)
