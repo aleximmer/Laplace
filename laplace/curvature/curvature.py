c3d8e158568ac45ddd25b456df4a07496d9b23fb
Ludvins <theludvins@gmail.com>
2024-06-10 15:01:25 +0200

    enable_backprop unused for last layer curvature

--- laplace/curvature/curvature.py
+++ laplace/curvature/curvature.py
@@ -161,7 +161,7 @@ class CurvatureInterface:
         if self.model.last_layer.bias is not None:
             Js = torch.cat([Js, identity], dim=2)
 
-        return Js, f
+        return (Js, f) if enable_backprop else (Js.detach(), f.detach())
 
     def gradients(self, x, y):
         """Compute batch gradients \\(\\nabla_\\theta \\ell(f(x;\\theta, y)\\) at
