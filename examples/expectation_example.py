acbeb2b70f87aeb504da9793ed29a76f5067163c
Agustinus Kristiadi <agustinus@kristia.de>
2025-02-04 13:05:28 -0500

    Update expectation example

--- examples/expectation_example.py
+++ examples/expectation_example.py
@@ -44,6 +44,8 @@ for pred_type in [PredType.GLM, PredType.NN]:
     f_sample = f_sample.squeeze(0)  # We only use a single sample
     print(f"TS shape: {f_sample.shape}, TS requires grad: {f_sample.requires_grad}")
 
+    # Get the gradient of the Thompson sample w.r.t. input x.
+    # Summed since it doesn't change the grad and autograd requires a scalar function.
     grad_x = autograd.grad(f_sample.sum(), x_test)[0]
 
     print(
@@ -53,7 +55,7 @@ for pred_type in [PredType.GLM, PredType.NN]:
 
 print()
 
-# Monte-Carlo expected improvement E_{f(x) ~ p(f(x) | D)} [max(f(x) - best_f, 0)]
+# Monte-Carlo expected improvement (EI): E_{f(x) ~ p(f(x) | D)} [max(f(x) - best_f, 0)]
 f_best = torch.tensor(0.123)  # Arbitrary in this example
 
 for pred_type in [PredType.GLM, PredType.NN]:
@@ -66,6 +68,8 @@ for pred_type in [PredType.GLM, PredType.NN]:
     ei = (f_samples - f_best.reshape(1, 1, 1)).clamp(0.0).mean(0)
     print(f"EI shape: {ei.shape}, EI requires grad: {ei.requires_grad}")
 
+    # Get the gradient of the EI w.r.t. input x.
+    # Summed since it doesn't change the grad and autograd requires a scalar function.
     grad_x = autograd.grad(ei.sum(), x_test)[0]
 
     print(
